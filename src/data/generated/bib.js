7c76e0a0449becf91d8beb80055696cdb6c007dc
mc13716327266 <mc13716327266@gmail.com>
2026-05-06 09:17:04 +0000

    Update SurVis data for CW3 literature review

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,114 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "du2023sentiment": {
+        "author": "Du, Yongping and Jin, Xingnan and Yan, Rui and Yan, Jingya",
+        "doi": "10.1016/j.ins.2023.01.098",
+        "journal": "Information Sciences",
+        "keywords": "E-commerce Marketing, Product Question Answering, Information Fusion",
+        "pages": "205--219",
+        "title": "Sentiment enhanced answer generation and information fusing for product-related question answering",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "volume": "627",
+        "year": "2023"
+    },
+    "huang2025hallucination": {
+        "author": "Huang, Lei and Yu, Weijiang and Ma, Weitao and Zhong, Weihong and Feng, Zhangyin and Wang, Haotian and Chen, Qianglong and Peng, Weihua and Feng, Xiaocheng and Qin, Bing and Liu, Ting",
+        "doi": "10.1145/3703155",
+        "journal": "ACM Transactions on Information Systems",
+        "keywords": "Hallucination and Verification, Large Language Models, Faithfulness",
+        "number": "2",
+        "pages": "1--55",
+        "title": "A Survey on Hallucination in Large Language Models: Principles, Taxonomy, Challenges, and Open Questions",
+        "type": "article",
+        "volume": "43",
+        "year": "2025"
+    },
+    "ji2023hallucination": {
+        "author": "Ji, Ziwei and Lee, Nayeon and Frieske, Rita and Yu, Tiezheng and Su, Dan and Xu, Yan and Ishii, Etsuko and Bang, Ye Jin and Madotto, Andrea and Fung, Pascale",
+        "doi": "10.1145/3571730",
+        "journal": "ACM Computing Surveys",
+        "keywords": "Hallucination and Verification, Natural Language Generation, Faithfulness",
+        "number": "12",
+        "pages": "1--38",
+        "title": "Survey of Hallucination in Natural Language Generation",
+        "type": "article",
+        "volume": "55",
+        "year": "2023"
+    },
+    "kshetri2024generative": {
+        "author": "Kshetri, Nir and Dwivedi, Yogesh K. and Davenport, Thomas H. and Panteli, Niki",
+        "doi": "10.1016/j.ijinfomgt.2023.102716",
+        "journal": "International Journal of Information Management",
+        "keywords": "E-commerce Marketing, AI Marketing, Generative AI",
+        "pages": "102716",
+        "title": "Generative artificial intelligence in marketing: Applications, opportunities, challenges, and research agenda",
+        "type": "article",
+        "volume": "75",
+        "year": "2024"
+    },
+    "ram2023incontext": {
+        "author": "Ram, Ori and Levine, Yoav and Dalmedigos, Itay and Muhlgay, Dor and Shashua, Amnon and Leyton-Brown, Kevin and Shoham, Yoav",
+        "doi": "10.1162/tacl_a_00605",
+        "journal": "Transactions of the Association for Computational Linguistics",
+        "keywords": "RAG and Retrieval, Retrieval-Augmented Generation, Language Models",
+        "pages": "1316--1331",
+        "title": "In-Context Retrieval-Augmented Language Models",
+        "type": "article",
+        "volume": "11",
+        "year": "2023"
+    },
+    "rodriguezdiaz2023semantic": {
+        "author": "Rodriguez-Diaz, Carlos A. and Jimenez, Sergio and Bejarano, Daniel and Bernal-Chavez, Julio A. and Gelbukh, Alexander",
+        "doi": "10.1016/j.ins.2023.118953",
+        "journal": "Information Sciences",
+        "keywords": "E-commerce Marketing, User-Generated Content, Product Description",
+        "pages": "118953",
+        "title": "Measuring semantic gap between user-generated content and product descriptions through compression comparison in e-commerce",
+        "type": "article",
+        "volume": "638",
+        "year": "2023"
+    },
+    "siriwardhana2023domain": {
+        "author": "Siriwardhana, Shamane and Weerasekera, Rivindu and Wen, Elliott and Kaluarachchi, Tharindu and Rana, Rajib and Nanayakkara, Suranga",
+        "doi": "10.1162/tacl_a_00530",
+        "journal": "Transactions of the Association for Computational Linguistics",
+        "keywords": "RAG and Retrieval, Domain Adaptation, Question Answering",
+        "pages": "1--17",
+        "title": "Improving the Domain Adaptation of Retrieval Augmented Generation (RAG) Models for Open Domain Question Answering",
+        "type": "article",
+        "volume": "11",
+        "year": "2023"
+    },
+    "tafesse2024chatgpt": {
+        "author": "Tafesse, Wondwesen and Wood, Bronwyn",
+        "doi": "10.1057/s41270-023-00284-w",
+        "journal": "Journal of Marketing Analytics",
+        "keywords": "E-commerce Marketing, Prompt Engineering, AI Marketing",
+        "pages": "790--805",
+        "title": "Hey ChatGPT: An examination of ChatGPT prompts in marketing",
+        "type": "article",
+        "volume": "12",
+        "year": "2024"
+    },
+    "wasilewski2025enhanced": {
+        "author": "Wasilewski, Adam and Chawla, Yash and Pralat, Ewa",
+        "doi": "10.1109/ACCESS.2025.3550956",
+        "journal": "IEEE Access",
+        "keywords": "E-commerce Marketing, AI Content Generation, Personalization",
+        "pages": "48083--48095",
+        "title": "Enhanced E-Commerce Personalization Through AI-Powered Content Generation Tools",
+        "type": "article",
+        "volume": "13",
+        "year": "2025"
+    },
+    "wasilewski2025personalized": {
+        "author": "Wasilewski, Adam",
+        "doi": "10.1016/j.csi.2025.104012",
+        "journal": "Computer Standards \\& Interfaces",
+        "keywords": "E-commerce Marketing, AI Content Generation, Product Description",
+        "pages": "104012",
+        "title": "Harnessing generative AI for personalized E-commerce product descriptions: A framework and practical insights",
+        "type": "article",
+        "volume": "94",
+        "year": "2025"
     }
 };
